f5a9428cc983b69ac39b34cc6a47f984ec46ad32
Eris <schafezr0000@gmail.com>
2025-06-22 22:58:17 -0700

    light theme in apps, and other fixes

--- apps/app.js
+++ apps/app.js
@@ -1,3 +1,73 @@
+const storage = (() => {
+    let storageData = {};
+    let storageName = 'supernova-data';
+
+    try {
+        storageData = JSON.parse(localStorage.getItem(storageName) || '{}');
+    } catch (e) {
+        console.error(e);
+    }
+
+    return {
+        get(key) {
+            return storageData[key];
+        },
+
+        set(key, value) {
+            storageData[key] = value;
+            localStorage.setItem(storageName, JSON.stringify(storageData));
+        },
+
+        delete(key) {
+            delete storageData[key];
+            localStorage.setItem(storageName, JSON.stringify(storageData));
+        },
+
+        all() {
+            return storageData;
+        },
+
+        clear() {
+            storageData = {};
+            localStorage.setItem(storageName, JSON.stringify(storageData));
+        },
+
+        settings: {
+            get(key) {
+                return storageData && storageData.settings && storageData.settings[key];
+            },
+
+            set(key, value) {
+                if (!storageData.settings) {
+                    storageData.settings = {};
+                }
+                storageData.settings[key] = value;
+                localStorage.setItem(storageName, JSON.stringify(storageData));
+            },
+
+            delete(key) {
+                if (storageData.settings) {
+                    delete storageData.settings[key];
+                    localStorage.setItem(storageName, JSON.stringify(storageData));
+                }
+            },
+
+            all() {
+                return storageData.settings || {};
+            },
+
+            clear() {
+                if (storageData.settings) {
+                    storageData.settings = {};
+                    localStorage.setItem(storageName, JSON.stringify(storageData));
+                }
+            }
+        },
+    };
+})();
+
+const settings = storage.settings;
+
 function reportHeight() {
   const height = document.body.scrollHeight;
   parent.postMessage(
@@ -6,6 +76,13 @@ function reportHeight() {
   );
 }
 
+function theme() {
+  if (settings.get('darkMode') == false) {
+    document.body.classList.add('light');
+  }
+}
+
 window.addEventListener('load', reportHeight);
+window.addEventListener('load', theme);
 
 new ResizeObserver(reportHeight).observe(document.body);
